a10732939537ee2be1d25eff7525ecba961bb1b8
harxshjoshi <dev.harishjoshi@gmail.com>
2022-09-08 08:20:06 +0545

    Bubble sort doc update

--- BubbleSort/src/com/harxsh/bubblesort/Main.java
+++ BubbleSort/src/com/harxsh/bubblesort/Main.java
@@ -8,15 +8,16 @@ public class Main {
         int[] arr = {20, 35, -15, 7, 55, 1, -22};
         System.out.println("Unsorted arr: " + Arrays.toString(arr));
 
-        /* At first:
-         * unsortedPartitionIndex = 6; this is the last index of the unsorted partition
-         * i = 0 to 6; is used to loop through array
-         * after every traverse the last element will be at sorted partition
-         * next traverse
-         * unsortedPartitionIndex = 5
-         * i = 0 to 5; (index 6 element is already at sorted partition)
-         * at the end of this traverse element at index 5 will be at sorted partition
-         * and so on.
+        /*
+         * Working:
+         * 1. First Iteration (Compare and Swap)
+         * i. Starting from the first index, compare he first and the second elements.
+         * ii. If the first element is greater than the second element, they are swapped.
+         * iii.Now, compare the second and the third elements. Swap then if they are not in order.
+         * iv. The above process goes on unit the last element.
+         * 2. Remaining Iteration
+         * i. The same process goes on for the remaining iterations.
+         * ii. After each iteration, the largest element among the unsorted elements is placed at the end.
          */
 
         // loop to access each array element
@@ -28,6 +29,8 @@ public class Main {
             for (int i = 0; i < lastUnsortedIndex; i++) {
                 // compare two adjacent elements
                 // swap if elements are not in the intended order
+                // to sort in descending order
+                // we need to change > to < while comparing
                 if (arr[i] > arr[i + 1]) {
                     swap(arr, i, i + 1);
                     swapped = true;
@@ -44,12 +47,11 @@ public class Main {
     }
 
     public static void swap(int[] arr, int i, int j) {
-        // if indexes are same return
+        // if indexes are same then no need to swap
         if (i == j)
             return;
 
-        // store one value to temp
-        // then swap the elements
+        // swap the elements in intended position
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
