c56fd77201f65efd41f6648c5e61bb0bdf2c6cde
harxshjoshi <dev.harishjoshi@gmail.com>
2022-09-08 07:22:16 +0545

    Bubble sort optimization

--- BubbleSort/src/com/harxsh/bubblesort/Main.java
+++ BubbleSort/src/com/harxsh/bubblesort/Main.java
@@ -10,18 +10,34 @@ public class Main {
 
         /* At first:
          * unsortedPartitionIndex = 6; this is the last index of the unsorted partition
-         * i = 0 to 6; index used to traverse the array from left to right
+         * i = 0 to 6; is used to loop through array
          * after every traverse the last element will be at sorted partition
          * next traverse
          * unsortedPartitionIndex = 5
-         * i = 0 to 5
+         * i = 0 to 5; (index 6 element is already at sorted partition)
          * at the end of this traverse element at index 5 will be at sorted partition
+         * and so on.
          */
+
+        // loop to access each array element
         for (int lastUnsortedIndex = arr.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+            // check if swapping occurs
+            boolean swapped = false;
+
+            // loop to compare array elements
             for (int i = 0; i < lastUnsortedIndex; i++) {
-                if (arr[i] > arr[i + 1])
+                // compare two adjacent elements
+                // swap if elements are not in the intended order
+                if (arr[i] > arr[i + 1]) {
                     swap(arr, i, i + 1);
+                    swapped = true;
+                }
             }
+
+            // no swapping means the array is already sorted
+            // so no need to for further comparison
+            if (!swapped)
+                break;
         }
 
         System.out.println("Sorted arr: " + Arrays.toString(arr));
